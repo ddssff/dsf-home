6db1526e93f89f570a2b51ca668e4226bc8981a4
David Fox <dsf@seereason.com>
2012-08-27 19:27:35 +0000

    Add a regex for recognizing ghc compiler errors

--- elisp/start.d/50haskell.el
+++ elisp/start.d/50haskell.el
@@ -1,28 +1,10 @@
 (remove-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
-;; Jeremy Shaw's Haskell setup
+(require 'compile)
+(set-default 'compilation-error-regexp-alist '(ghc))
+(add-to-list 'compilation-error-regexp-alist-alist
+  '(ghc "^\\(\\|               defined at \\|               imported from `[^']*\' at \\|               \(and originally defined at \\)\\([^ 	\n]+\\):\\([0-9]+\\):\\([0-9]+\\):?$" 2 3 4))
 
-(setq auto-mode-alist
-      (append auto-mode-alist
-              '(("\\.[hg]s$"  . haskell-mode)
-                ("\\.hic?$"     . haskell-mode)
-		("\\.chs$"    . haskell-mode)
-                ("\\.l[hg]s$" . literate-haskell-mode))))
-(autoload 'haskell-mode "haskell-mode"
-  "Major mode for editing Haskell scripts." t)
-(autoload 'literate-haskell-mode "haskell-mode"
-  "Major mode for editing literate Haskell scripts." t)
-
-(add-hook 'haskell-mode-hook 'turn-on-haskell-font-lock)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-;(remove-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-;(add-hook 'haskell-mode-hook 'turn-on-haskell-hugs)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-ghci)
-(add-hook 'haskell-mode-hook 
-	  (function
-	   (lambda ()
-	     (setq haskell-ghci-program-name "ghci6")
-	     (setq haskell-ghci-program-args '("-fallow-overlapping-instances" "-fcontext-stack=30" "-fglasgow-exts" "-farrows" "-ddump-splices" "-fallow-undecidable-instances")))))
+(add-hook 'darcs-dired-mode-hook
+       (lambda () (setq compile-command "runhaskell Setup build")))
