b44499714dbec6fd438b61055b5c9cd5163f63c6
David Fox <ddssff@gmail.com>
2015-03-21 07:42:09 -0700

    Disable my custom ghc error message parsing code

--- elisp/start.d/50haskell.el
+++ elisp/start.d/50haskell.el
@@ -2,9 +2,9 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
 (require 'compile)
-(set-default 'compilation-error-regexp-alist '(ghc))
-(add-to-list 'compilation-error-regexp-alist-alist
-  '(ghc "^\\(\\|               defined at \\|               imported from `[^']*\' at \\|               \(and originally defined at \\)\\([^ 	\n]+\\):\\([0-9]+\\):\\([0-9]+\\):?$" 2 3 4))
+;(set-default 'compilation-error-regexp-alist '(ghc))
+;(add-to-list 'compilation-error-regexp-alist-alist
+;  '(ghc "^\\(\\|               defined at \\|               imported from `[^']*\' at \\|               \(and originally defined at \\)\\([^ 	\n]+\\):\\([0-9]+\\):\\([0-9]+\\):?$" 2 3 4))
 
 (add-hook 'darcs-dired-mode-hook
        (lambda () (setq compile-command "runhaskell Setup build")))
