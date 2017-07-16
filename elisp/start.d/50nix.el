4a7358f613c35ce030776482bfd8ef7b7565e1a4
David Fox <dsf@seereason.com>
2017-07-16 11:06:18 -0700

    Comment out nix stuff until it is straigtened out

--- elisp/start.d/50nix.el
+++ elisp/start.d/50nix.el
@@ -1,4 +1,4 @@
 (setenv "PAGER" "cat") ;; don't try to use less/more in M-x shell
 (setenv "NIX_REMOTE_SYSTEMS" "/etc/nix/machines") ;; will be used when we have distributed builds
-(add-to-list 'load-path "~/.nix-profile/share/emacs/site-lisp") ;; so we can find `nix-mode`
-(require 'nix-mode) ;; for editting .nix files. Hopefully available even if you have not installed emacs through nix
+;(add-to-list 'load-path "~/.nix-profile/share/emacs/site-lisp") ;; so we can find `nix-mode`
+;(require 'nix-mode) ;; for editting .nix files. Hopefully available even if you have not installed emacs through nix
